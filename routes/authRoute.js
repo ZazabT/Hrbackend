88a0894373409a7575cf61be392edc22aede7e77
Tizazab <tizazabayana2@gmail.com>
2025-04-05 04:11:48 -0700

    Added Login and limiter rate

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,17 +1,30 @@
 import express from 'express';
-import {login , logout ,register } from '../controllers/authController.js';
+import {login , logout ,register ,refreshAccessToken } from '../controllers/authController.js';
 import {verifyToken} from '../middleware/authMiddlewate.js'
+import rateLimit from 'express-rate-limit';
+
 
 const route = express.Router();
 
+
+// Set rate limit (e.g., 5 requests per minute)
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,  // 1 minute
+    max: 5,  // Allow 5 requests per minute
+    message: 'Too many requests from this IP, please try again later',
+});
+
 // Login Route
-route.get('/login' ,login);
+route.post('/login' , limiter ,login );
 
 // Register Route
-route.get('/register' ,register);
+route.post('/register' ,register);
 
 // Logout Route
-route.get('/logout' ,logout , verifyToken);
+route.post('/logout' ,logout , verifyToken);
+
+// Refresh token
+route.post('//refresh-token' , refreshAccessToken)
 
 
 
